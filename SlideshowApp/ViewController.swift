d15d6b2640f5154a8d4d8206ba61caafe866b86d
天野智広 <amanotomohiro@amanotomohiro-no-macbook-pro.local>
2019-01-31 07:11:59 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 天野智広 on 2019/01/31.
+//  Copyright © 2019 天野智広. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
